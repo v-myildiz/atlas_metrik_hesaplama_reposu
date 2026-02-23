d27c56c5397562b49aa3ab98a44d3474a79586ae
v-myildiz <myildiz@valven.com>
2026-02-23 08:34:04 +0300

    Add initial content to *.config.ts

---
+++ *.config.ts
@@ -0,0 +1,20 @@
+
+bu dosya insert edilmemeli
+
+bu dosya insert edilmemeli
+bu dosya insert edilmemeli
+bu dosya insert edilmemeli
+
+v
+bu dosya insert edilmemeli
+bu dosya insert edilmemeli
+bu dosya insert edilmemelibu dosya insert edilmemeli
+bu dosya insert edilmemeli
+
+bu dosya insert edilmemeli
+bu dosya insert edilmemeli
+bu dosya insert edilmemeli
+bu dosya insert edilmemeli
+bu dosya insert edilmemeli
+bu dosya insert edilmemeli
+bu dosya insert edilmemelibu dosya insert edilmemeli
